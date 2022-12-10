bc9814c0c2ac06b827766d8c07ef081e1223c6e0
Phuchandsome <phucnguyenksqt11@gmail.com>
2022-12-10 09:59:08 +0700

    oke

--- API-Test/src/main/java/com/example/apitest/service/user/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.apitest.service.user;
-
-import com.example.apitest.enity.User;
-import com.example.apitest.service.IGeneralService;
-
-public interface IUserService extends IGeneralService<User> {
-
-}
